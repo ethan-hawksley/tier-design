94bb4e807cf060b5e8d667868a28feb372271dd6
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-28 18:57:23 +0000

    refactor: update HTML meta charset and clean up main.ts and style.css

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import tierDesignLogo from './tier-design.svg';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-      <img src="${tierDesignLogo}" alt="Tier Design Logo"   />
-      <h1 >Tier Design</h1>
-      <p >A minimal Vite + TypeScript + Tailwind setup.</p>
+      <img src="${tierDesignLogo}" alt="Tier Design Logo" />
+      <h1>Tier Design</h1>
   </div>
 `;
