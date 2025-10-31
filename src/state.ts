cf20a1044e41bb878b82a6cec4bd1caca1a86b33
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-10-31 10:18:22 +0000

    feat: clear initial items and unrankedItems for a fresh state setup

--- src/state.ts
+++ src/state.ts
@@ -13,29 +13,13 @@ export const state: AppState = {
       id: 0,
       label: 'S',
       colour: '#ff7f80',
-      items: [
-        { id: 1, type: 'text', text: 'Item 1' },
-        { id: 2, type: 'text', text: 'Item 2' },
-        { id: 3, type: 'text', text: 'Item 3' },
-        { id: 4, type: 'text', text: 'Item 4' },
-        { id: 5, type: 'text', text: 'Item 5' },
-        { id: 6, type: 'text', text: 'Item 6' },
-        { id: 7, type: 'text', text: 'Item 7' },
-      ],
+      items: [],
     },
     {
       id: 1,
       label: 'A',
       colour: '#ffc07f',
-      items: [
-        { id: 11, type: 'text', text: 'Item 11' },
-        { id: 12, type: 'text', text: 'Item 12' },
-        { id: 13, type: 'text', text: 'Item 13' },
-        { id: 14, type: 'text', text: 'Item 14' },
-        { id: 15, type: 'text', text: 'Item 15' },
-        { id: 16, type: 'text', text: 'Item 16' },
-        { id: 17, type: 'text', text: 'Item 17' },
-      ],
+      items: [],
     },
     {
       id: 2,
@@ -62,10 +46,5 @@ export const state: AppState = {
       items: [],
     },
   ],
-  unrankedItems: [
-    { id: 0, type: 'text', text: 'Item 0' },
-    { id: 8, type: 'text', text: 'Item 8' },
-    { id: 9, type: 'text', text: 'Item 9' },
-    { id: 10, type: 'text', text: 'Item 10' },
-  ],
+  unrankedItems: [],
 };
